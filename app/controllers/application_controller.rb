0628dbbcacbde40e9c96e11718b1fe21ff26b4f6
Koki Matsui <irb339231@gmail.com>
2026-07-05 09:27:36 +0900

    Add redirect paths sing in/out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end 
 end
