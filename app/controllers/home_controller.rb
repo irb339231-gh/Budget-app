9e39b8db784003c732be43a864d96bb3c11f6474
Koki Matsui <irb339231@gmail.com>
2026-07-13 06:57:05 +0900

    Add:算術ロジックの実装＆表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @available_amount = current_user.available_amount
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def available_amount
+    total_income - total_future_expenses - total_fixed_costs
+  end
+
+  def job_search_months
+    return 0 if job_search_start_month.nil? || job_search_end_month.nil?
+    ((job_search_end_month.year - job_search_start_month.year) * 12 +
+      (job_search_end_month.month - job_search_start_month.month)).abs
+  end
+  
+  def total_income
+    expenditures.where(category: :income).sum(:amount)
+  end
+
+  def total_future_expenses
+    expenditures.where(category: :future_expense).sum(:amount)
+  end
+
+  def total_fixed_costs
+    expenditures.where(category: :fixed_cost).sum(:amount) * job_search_months
+  end
 end
