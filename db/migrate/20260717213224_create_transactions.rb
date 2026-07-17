bbc5a58c0996f66c3ab6453722d17789fa351f20
Koki Matsui <irb339231@gmail.com>
2026-07-18 07:14:02 +0900

    Fix RuboCop offenses

--- db/migrate/20260717213224_create_transactions.rb
+++ db/migrate/20260717213224_create_transactions.rb
@@ -3,7 +3,7 @@ class CreateTransactions < ActiveRecord::Migration[8.1]
     create_table :transactions do |t|
       t.references :user, null: false, foreign_key: true
       t.string :name
-      t.integer :category, null: false 
+      t.integer :category, null: false
       t.integer :amount, null: false
 
       t.timestamps
