d52026d7375b6c4aa52d17286b25000c350bd46c
Koki Matsui <irb339231@gmail.com>
2026-07-29 09:58:06 +0900

    test: Minitestのファイルを削除

--- test/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name { "テストユーザー" }
-    sequence(:email) { |n| "test#{n}@example.com" }
-    password { "password123" }
-    password_confirmation { "password123" }
-  end
-end
