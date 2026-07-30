147b47fea8ef6b40dd67b561f52e71c42311d70c
Koki Matsui <irb339231@gmail.com>
2026-07-30 13:38:07 +0900

    style:RuboCopのエラー修正

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://6ba979e149829f487ecd9bf78d407688@o4511822158168064.ingest.us.sentry.io/4511822164721664'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
