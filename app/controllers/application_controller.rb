0487a1ad13bdcbefb720b6ac1444f50e48722c48
Koki Matsui <irb339231@gmail.com>
2026-07-31 15:46:10 +0900

    feat: ウィザード形式の初期設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
+
   allow_browser versions: :modern
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || home_path
+    if resource.job_search_start_month.nil?
+      wizard_path(:step1)
+    else
+      stored_location_for(resource) || home_path
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
---
+++ app/controllers/wizard_controller.rb
@@ -0,0 +1,54 @@
+class WizardController < ApplicationController
+  include Wicked::Wizard
+  before_action :authenticate_user!
+
+  steps :step1, :step2, :step3, :step4
+
+  def show
+    @user = current_user
+    render_wizard
+  end
+
+  def update
+    @user = current_user
+    case step
+    when :step1
+      @user.update(job_search_params)
+    when :step2
+      @expenditure = current_user.expenditures.build(income_params)
+      @expenditure.category = :income
+      @expenditure.save
+    when :step3
+      @expenditure = current_user.expenditures.build(future_expense_params)
+      @expenditure.category = :future_expense
+      @expenditure.save
+    when :step4
+        @expenditure = current_user.expenditures.build(fixed_cost_params)
+        @expenditure.category = :fixed_cost
+        @expenditure.save
+    end
+    render_wizard @user
+  end
+
+  private
+
+  def job_search_params
+    params.require(:user).permit(
+      :job_search_start_month,
+      :job_search_end_month
+    )
+  end
+
+  def income_params
+    params.require(:income).permit(:name, :amount)
+  end
+
+  def future_expense_params
+    params.require(:future_expense).permit(:name, :amount)
+  end
+
+  def fixed_cost_params
+    params.require(:fixed_cost).permit(:name, :amount)
+  end
+
+end
