dda5eb1e85fa75d8f670e2c1ff44f4653cf3aaf9
Koki Matsui <irb339231@gmail.com>
2026-07-31 13:52:21 +0900

    feat:日毎・週毎・月毎の使える金額を算出・表示機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,26 @@ class User < ApplicationRecord
     total_income - total_future_expenses - total_fixed_costs - total_transactions_expenses + total_transactions_incomes
   end
 
+  def daily_available_amount
+    return 0 if job_search_days.zero? #daysが0の場合は除く
+    available_amount / job_search_days
+  end
+
+  def weekly_available_amount
+    return 0 if job_search_days.zero? #daysが0の場合は除く
+    available_amount / (job_search_days / 7) #転職活動期間を7で割る
+  end
+
+  def monthly_available_amount
+    return 0 if job_search_months.zero? #monthsが0の場合は除く
+    available_amount / job_search_months
+  end
+
+  def job_search_days
+    return 0 if job_search_start_month.nil? || job_search_end_month.nil?
+    (job_search_end_month - job_search_start_month ).to_i
+  end
+
   def job_search_months
     return 0 if job_search_start_month.nil? || job_search_end_month.nil?
     ((job_search_end_month.year - job_search_start_month.year) * 12 +
