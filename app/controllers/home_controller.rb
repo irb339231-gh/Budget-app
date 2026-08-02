abbe75826cecf8090a44c621a8ceb596e7b2b5b9
Koki Matsui <irb339231@gmail.com>
2026-08-02 10:24:03 +0900

    feature:残高の割合によって今使える金額の背景色を変更する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,5 +6,6 @@ class HomeController < ApplicationController
     @daily_available_amount = current_user.daily_available_amount
     @weekly_available_amount = current_user.weekly_available_amount
     @monthly_available_amount = current_user.monthly_available_amount
+    @available_amount_percentage = current_user.available_amount_percentage
   end
 end
