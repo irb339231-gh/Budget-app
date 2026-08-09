258a1e6e4bbc0389be12c6b8bee3ec88254018f0
Koki Matsui <irb339231@gmail.com>
2026-08-09 11:02:16 +0900

    feat:アカウントロック機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :lockable
 
   def available_amount
     total_income - total_future_expenses - total_fixed_costs - total_transactions_expenses + total_transactions_incomes
