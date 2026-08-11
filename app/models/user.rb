a1b6296af5bd27bb111f0cb9156e090d39b99b70
Koki Matsui <irb339231@gmail.com>
2026-08-11 11:02:43 +0900

    fix: パスワードチェックのをテストから除外

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 20 }
-  validates :password, pwned: true, if: :password_required?
+  validates :password, pwned: true, if: -> { password_required? && !Rails.env.test? }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
