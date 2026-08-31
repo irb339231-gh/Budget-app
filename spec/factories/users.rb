98ad8c2fc39e88aadcd369f633e986f92f279c71
Koki Matsui <irb339231@gmail.com>
2026-08-31 17:46:21 +0900

    fix:testのパスワードを15に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "テストユーザー" }
     email { "test_#{SecureRandom.uuid}@example.com" }
-    password { "ikurahaikaga12" }
-    password_confirmation { "ikurahaikaga12" }
+    password { "ikurahaikaga123" }
+    password_confirmation { "ikurahaikaga123" }
   end
 end
